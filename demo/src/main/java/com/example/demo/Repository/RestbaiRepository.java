269dc3b605712991ba33b811d5db88f932c051d6
orio1pp <oriol.portell@estudiantat.upc.edu>
2023-05-12 22:47:36 +0200

    adding rest template to attack api

--- demo/src/main/java/com/example/demo/Repository/RestbaiRepository.java
+++ demo/src/main/java/com/example/demo/Repository/RestbaiRepository.java
@@ -1,4 +1,11 @@
 package com.example.demo.Repository;
 
+import org.springframework.web.client.RestTemplate;
+
 public class RestbaiRepository {
+    private final RestTemplate restTemplate  = new RestTemplate();
+    private final String apiKey = "de16fead467ec83f535085d69472c4e77e3ba8edf6f7b43f994c70cd3ceb9571";
+
+    private final String baseUrl = "https://api-us.restb.ai/vision/v2/multipredict";
+    
 }
