3718d92c3ba696defc7a92030127d0aa081cc623
HolaholitaDavid <holaholitadavid@gmail.com>
2023-05-12 23:57:18 +0200

    merged

--- demo/src/main/java/com/example/demo/Repository/RestbaiRepository.java
+++ demo/src/main/java/com/example/demo/Repository/RestbaiRepository.java
@@ -4,20 +4,19 @@ import com.example.demo.Models.QualityImage;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import static com.example.demo.Utils.ConstURLS.base_url;
-import static com.example.demo.Utils.ConstURLS.formatURL;
+import static com.example.demo.Utils.ConstURLS.*;
 
 public class RestbaiRepository {
     private final RestTemplate restTemplate  = new RestTemplate();
 
     public QualityImage getQualityImageFromUrl(String imageUrl){
-        String finalUrl = formatURL("re_condition_r1r6_international", imageUrl);
+        String finalUrl = formatURL(SCORE, imageUrl);
         ResponseEntity<QualityImage> response = restTemplate.getForEntity(finalUrl, QualityImage.class);
         return response.getBody();
     }
 
     public QualityImage enhanceImage(String imageUrl) {
-        String finalUrl = base_url.replace("{REMAINING}", "?model_id=auto_enhancement&image_url="+imageUrl);
+        String finalUrl = formatURL(ENHANCE, imageUrl);
         ResponseEntity<QualityImage> response = restTemplate.getForEntity(finalUrl, QualityImage.class);
         return response.getBody();
     }
